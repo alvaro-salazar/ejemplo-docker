238941f62ddc656b0eac583442399161a2335c43
Alvaro Salazar <ah.salazar@uniandes.edu.co>
2024-04-09 23:11:50 -0500

    ejemplo con impresion en bucle

--- app.py
+++ app.py
@@ -2,8 +2,8 @@ import time
 from datetime import datetime
 
 print("Bienvenido al codelab de docker")
-# while True:
-#  now = datetime.now()
-#  current_time = now.strftime("%H:%M:%S")
-#  print(f"Se ha consultado el log del contenedor a las {current_time}")
-#  time.sleep(3)
+while True:
+  now = datetime.now()
+  current_time = now.strftime("%H:%M:%S")
+  print(f"Se ha consultado el log del contenedor a las {current_time}")
+  time.sleep(3)
